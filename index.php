0094a900b271919c35b2c470cec4b94f0829ce06
Mfilou <mourainirufin@gmail.com>
2023-09-19 09:37:29 +0100

    test-devops-php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma Première App PHP</title>
+</head>
+<body>
+    <h1><?php echo "Hello, this is a Devops's job on php project"; ?></h1>
+</body>
+</html>
